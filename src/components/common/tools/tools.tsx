fbb539572a70397c987ddebb347736254baa274c
elijahnikov <enkv@proton.me>
2024-04-11 14:26:05 +0100

    adjust gradient width

--- src/components/common/tools/tools.tsx
+++ src/components/common/tools/tools.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { toolsMap } from "@/lib/tools";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
 
 export default function Tools() {
+  const isDesktop = useMediaQuery("(min-width: 660px)");
   return (
     <div className="flex flex-col justify-center">
       <h1 className="mb-4 text-lg font-semibold">Apps I like using 🔬</h1>
       <div className="relative  h-[140px] w-full overflow-hidden bg-white">
-        <Marquee gradient gradientWidth={200} className="-mt-8" speed={70}>
+        <Marquee
+          gradient
+          gradientWidth={isDesktop ? 200 : 100}
+          className="-mt-8"
+          speed={70}
+        >
           <div className="flex h-[200px] items-center space-x-[70px]">
             {toolsMap.map((tool) => (
               <div
