ad8cc07edd500e21ac531d815c007aa444da9e74
elijahnikov <enkv@proton.me>
2024-04-15 07:12:02 +0100

    finalising expanded view

--- src/components/common/pages/projects-page/single-project-expanded.tsx
+++ src/components/common/pages/projects-page/single-project-expanded.tsx
@@ -1,9 +1,9 @@
 import { Badge } from "@/components/ui/badge";
 import { buttonVariants } from "@/components/ui/button";
-import { Separator } from "@/components/ui/separator";
 import { type ProjectsType } from "@/lib/data/projects";
 import { cn } from "@/lib/utils";
 import { ExternalLink } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 import { BsGithub } from "react-icons/bs";
 
@@ -13,43 +13,80 @@ export default function SingleProjectExpanded({
   project: ProjectsType;
 }) {
   return (
-    <div className="w-full px-6 py-4">
+    <div className="w-full px-10 py-4">
+      {/* header */}
       <div className="flex justify-between">
         <div className="w-full">
-          <h1 className="-mb-2 text-[48px] font-bold">{project.title}</h1>
+          <div className="flex items-center">
+            <h1 className="-mb-2 text-[48px] font-bold">{project.title}</h1>
+            <div>{project.wip && <Badge className="ml-2 mt-6">WIP</Badge>}</div>
+          </div>
           <p className="text-md w-[75%] text-neutral-600">
             {project.description}
           </p>
         </div>
         <div className="flex justify-between">
-          <Separator orientation="vertical" className="bg-neutral-200" />
-          <p className={cn("my-4 w-[75%] text-sm")}>
+          <p className={cn("my-4 w-[80%] text-sm text-neutral-700")}>
             {project.verbose_description}
           </p>
-          <Separator orientation="vertical" className="bg-neutral-200" />
-          <div
-            onClick={(e) => {
-              e.stopPropagation();
-            }}
-            className="flex space-x-2"
-          >
-            <div>{project.wip && <Badge>WIP</Badge>}</div>
-            <Link
-              href={project.github_url}
-              className={cn(buttonVariants({ size: "icon" }), "h-8 w-8")}
-              target="_blank"
-            >
-              <BsGithub size={16} />
-            </Link>
-            <Link
-              href={project.url}
-              className={cn(buttonVariants({ size: "icon" }), "h-8 w-8")}
-              target="_blank"
-            >
-              <ExternalLink size={16} />
-            </Link>
-          </div>
         </div>
+      </div>
+      {/* images */}
+      <div className="mt-5 space-y-10 ">
+        {project.images?.map((imgs) => (
+          <div key={imgs.id}>
+            <div className="flex w-full justify-center" key={imgs.id}>
+              <div className="w-[80%]">
+                <div className="flex items-center">
+                  <div className="flex">
+                    <Badge className="p- w-4 items-center justify-center rounded-full bg-neutral-900 text-white">
+                      {imgs.id}
+                    </Badge>
+                  </div>
+                  <h1 className="mb-2 mt-2 px-2 text-lg font-semibold">
+                    {imgs.title}
+                  </h1>
+                </div>
+                <Image
+                  src={imgs.src}
+                  alt={imgs.label}
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: "100%", height: "auto" }}
+                  aria-label={imgs.label}
+                  className="mx-auto flex justify-center rounded-xl border border-neutral-200 shadow-sm"
+                />
+                <p className="px-2 py-1 text-xs text-neutral-500">
+                  {imgs.label}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        className="mx-auto mb-[50px] mt-[100px] flex w-[80%] space-x-2"
+      >
+        <Link
+          href={project.github_url}
+          className={cn(buttonVariants({ size: "lg" }), "w-full space-x-2")}
+          target="_blank"
+        >
+          <BsGithub size={16} />
+          <p>GitHub</p>
+        </Link>
+        <Link
+          href={project.url}
+          className={cn(buttonVariants({ size: "lg" }), "w-full space-x-2")}
+          target="_blank"
+        >
+          <ExternalLink size={16} />
+          <p>URL</p>
+        </Link>
       </div>
     </div>
   );
--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -8,6 +8,7 @@ export type ProjectsType = {
   github_url: string;
   url: string;
   images?: Array<{
+    id: number;
     src: string;
     title: string;
     label: string;
@@ -26,5 +27,35 @@ export const projects: ProjectsType[] = [
     github_url: "https://github.com/elijahnikov/gridl",
     url: "https://gridl-gold.vercel.app",
     wip: true,
+    images: [
+      {
+        id: 1,
+        src: "https://utfs.io/f/85d66cb9-d8ea-431f-9264-40747de5c360-kgv9bp.png",
+        title: "Moodboard",
+        label:
+          "Main moodboard that is shown to users who navigate to a user's gridl link.",
+      },
+      {
+        id: 2,
+        src: "https://utfs.io/f/33e938a8-2d5c-4fa5-adaa-f9b440f79a6a-jr5rse.png",
+        title: "Links",
+        label:
+          "Links page showing all links with a powerful filtering system and link tracking for each link.",
+      },
+      {
+        id: 3,
+        src: "https://utfs.io/f/849aebb4-9be5-4bfe-9ea8-43adc3d1d0ed-nhw8c4.png",
+        title: "Editor",
+        label:
+          "Editor for gridl moodboards; change background, create links, reorder and resize links.",
+      },
+      {
+        id: 4,
+        src: "https://utfs.io/f/8bb328f6-5afe-4758-90a8-ab1c7502aac0-537b8h.png",
+        title: "Analytics",
+        label:
+          "Analytics for gridl moodboards, showing clicks over time and where said links occurred.",
+      },
+    ],
   },
 ];
